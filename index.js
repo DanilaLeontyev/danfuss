d7d5df54dc24f03e24b8ca21090f40e7a430ddcb
Danila Leontyev <danila.leontyev.web@gmail.com>
2017-08-08 19:16:23 +0500

    add animation for apartments list

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ function animation() {
     const forHomeTitle = document.getElementById("forHome--title");
     const forApartmentTitle = document.getElementById("forApartment--title");
 
-    const forHomeList1 = document.getElementById('list--item_1');
-    const forHomeList2 = document.getElementById('list--item_2');
-    const forHomeList3 = document.getElementById('list--item_3');
-    const forHomeList4 = document.getElementById('list--item_4');
+    const forHomeList = document.getElementById('forHome--list');
+    const forHomeList1 = document.getElementById('homeList--item_1');
+    const forHomeList2 = document.getElementById('homeList--item_2');
+    const forHomeList3 = document.getElementById('homeList--item_3');
+    const forHomeList4 = document.getElementById('homeList--item_4');
+
+    const forApartmentsList = document.getElementById('forApartments--list');
+    const forApartmentsList1 = document.getElementById('apartmentsList--item_1');
+    const forApartmentsList2 = document.getElementById('apartmentsList--item_2');
+    const forApartmentsList3 = document.getElementById('apartmentsList--item_3');
+    const forApartmentsList4 = document.getElementById('apartmentsList--item_4');
 
     container.onclick = function (ev) {
         let target = event.target;
@@ -29,6 +36,11 @@ function animation() {
     }
 
     function animateHome(node) {
+        forHome.style.display = 'block';
+        forHomeTitle.style.display = 'block';
+        forHomeList.style.display = 'block';
+
+        
         Velocity(forHome,
             {
                 width: "80%"
@@ -51,14 +63,24 @@ function animation() {
                 marginTop: '350px'
             },
             {
-                duration: 1000
+                duration: 1000,
+                display: 'none'
             });
         Velocity(forApartmentTitle,
             {
                 opacity: 0
             },
             {
-                duration: 1000
+                duration: 1000,
+                display: 'none'
+            });
+        Velocity(forApartmentsList,
+            { 
+                opacity: 0 
+            },
+            {
+                duration: 0,
+                display: 'none'
             });
         Velocity(containerTitle,
             {
@@ -66,7 +88,8 @@ function animation() {
                 marginTop: '110px'
             },
             {
-                duration: 1000
+                duration: 1000,
+                display: 'none'
             });
         Velocity(forHomeSq,
             {
@@ -144,6 +167,94 @@ function animation() {
             },
             {
                 duration: 2000
+            })
+        Velocity(forHomeSq,
+            {
+                opacity: 0,
+                marginTop: '350px'
+            },
+            {
+                duration: 1000,
+                display: 'none'
+            });
+        Velocity(forHomeTitle,
+            {
+                opacity: 0
+            },
+            {
+                duration: 1000,
+                display: 'none'
+            });
+        Velocity(containerTitle,
+            {
+                opacity: 0,
+                marginTop: '110px'
+            },
+            {
+                duration: 1000,
+                display: 'none'
+            });
+        Velocity(forApartmentSq,
+            {
+                opacity: 1,
+                marginTop: '100px',
+                marginLeft: '50px'
+            },
+            {
+                duration: 1000,
+                delay: 300,
+                display: 'block'
+            });
+        Velocity(forApartmentTitle,
+            {
+                opacity: 1,
+                marginTop: "-90px",
+                marginLeft: "200px"
+            },
+            {
+                duration: 1000,
+                delay: 300,
+                display: 'block'
+            });
+        Velocity(forApartmentsList4,
+            {
+                opacity: 1,
+                top: 0
+            },
+            {
+                duration: 1000,
+                display: 'block',
+                delay: 400
+            })
+        Velocity(forApartmentsList3,
+            {
+                opacity: 1,
+                top: 0
+            },
+            {
+                duration: 1000,
+                display: 'block',
+                delay: 600
+            })
+        Velocity(forApartmentsList2,
+            {
+                opacity: 1,
+                top: 0
+            },
+            {
+                duration: 1000,
+                display: 'block',
+                delay: 800
+            })
+        Velocity(forApartmentsList1,
+            {
+                opacity: 1,
+                top: 0
+            },
+            {
+                duration: 1000,
+                display: 'block',
+                delay: 1000
             })
     }
 }
